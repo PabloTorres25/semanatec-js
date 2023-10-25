414a451216ef1de852e2d5435763fadf5a22e897
PabloTorres25 <a01753412@tec.mx>
2023-10-25 19:56:31 +0000

    2023-10-25 01:56pm

--- controllers/persona.js
+++ controllers/persona.js
@@ -3,5 +3,6 @@ const sequelize = require('../util/database');
 const Persona = sequelize.models.persona;
 
 exports.postAgregarPersona=(req, res)=>{
-    console.log
+    console.log(req.body);
+    Persona.create(req.body);
 }
