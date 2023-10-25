9e70d3132b6df0058cb97bdf0387cafbee641767
PabloTorres25 <a01753412@tec.mx>
2023-10-25 19:22:44 +0000

    2023-10-25 01:22pm

--- util/relations.js
+++ util/relations.js
@@ -3,10 +3,13 @@ function applyRelations(sequelize){
     const Proyecto = sequelize.models.proyecto;
     const Persona = sequelize.models.persona;
     const ProyectoPersona = sequelize.models.proyectopersona;
-}
 
 // Un proyecto pueden estar muchas personas
 Proyecto.belongsToMany(Persona);
 
 // Una persona puede participar en muchos proyectos
 Persona.belongsToMany(Proyecto);
+
+}
+
+module.exports = {applyRelations};
