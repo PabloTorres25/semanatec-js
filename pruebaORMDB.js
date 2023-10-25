1795e0ebd310fcd36cc3c31cf350ec2acc5d7da0
PabloTorres25 <a01753412@tec.mx>
2023-10-25 17:39:03 +0000

    2023-10-25 11:39am

---
+++ pruebaORMDB.js
@@ -0,0 +1,7 @@
+// Como utilizar la herramienta ORM Sequelize
+const Sequelize=require('sequelize');
+// Configurar la conexión
+// mysql -u admin -h -P 3306 -p
+const sequelize = new Sequelize('semanatec', 'admin', 'password', {
+    
+})
