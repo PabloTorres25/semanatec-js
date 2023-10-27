a9367fbbfa4547006ea565d2409d926ed48e4f61
PabloTorres25 <a01753412@tec.mx>
2023-10-27 16:28:14 +0000

    2023-10-27 10:28am

--- routes/persona.js
+++ routes/persona.js
@@ -2,3 +2,5 @@
 const express = require('express');
 const router = express.Router();
 const personaController = require("../controllers/persona");    // Lo que hicimos del modulo exports, es para que pdodamos usarlo en esta linea
+
+router.post('/agregarPersona', personaController.postAgregarPersona);
