d2046d7d924bef720af15de32d3799e88eaec67f
PabloTorres25 <a01753412@tec.mx>
2023-10-27 16:27:19 +0000

    2023-10-27 10:27am

--- routes/persona.js
+++ routes/persona.js
@@ -1,4 +1,4 @@
 // Este va a tener la ruta y va a conectar al controller, y de ahi lo conectaremos al index
 const express = require('express');
 const router = express.Router();
-const personaController = require("../controllers/persona");
+const personaController = require("../controllers/persona");    // Lo que hicimos del modulo exports, es para que pdodamos usarlo en esta linea
